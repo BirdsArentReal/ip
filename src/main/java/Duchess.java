d23346fc76a92158ba3facf4b176f236c065f0db
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-08-18 16:33:56 +0800

    Extract greeting into separate functions

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,12 +1,24 @@
 public class Duchess {
     public static void main(String[] args) {
+        greet();
+    }
+
+    private static void greet() {
+        final String name = "Duchess";
+        printBanner();
+        System.out.println("Hello! I am " + name + ".");
+        System.out.println("What can I do for you?");
+
+    }
+
+    private static void printBanner() {
         String banner =
-                  " ____  _   _  _____    _ ____  ___  ___\n"
-                + "|  _ \\| | | |/ __/ |  | | ___|/   \\/   \\\n"
-                + "| | | | | | | |  | |__| | |__|  | |  | |\n"
-                + "| | | | | | | |  |  __  |  __|\\  \\/\\  \\/\n"
-                + "| |_| | \\_/ | |__| |  | | |__ /\\  \\/\\  \\\n"
-                + "|____/ \\___/ \\___\\_|  |_|____|\\___/\\___/\n";
+                " ____  _   _  _____    _ ____  ___  ___\n"
+                        + "|  _ \\| | | |/ __/ |  | | ___|/   \\/   \\\n"
+                        + "| | | | | | | |  | |__| | |__|  | |  | |\n"
+                        + "| | | | | | | |  |  __  |  __|\\  \\/\\  \\/\n"
+                        + "| |_| | \\_/ | |__| |  | | |__ /\\  \\/\\  \\\n"
+                        + "|____/ \\___/ \\___\\_|  |_|____|\\___/\\___/\n";
         System.out.println(banner);
     }
 }
