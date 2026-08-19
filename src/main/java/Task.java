f8ae686496a18e2be0c33233a5bb2388e0a7bda9
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-08-19 17:55:51 +0800

    Remove unused functions in Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,14 +7,6 @@ public class Task {
         this.done = false;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public boolean isDone() {
-        return done;
-    }
-
     public void mark() {
         this.done = true;
     }
