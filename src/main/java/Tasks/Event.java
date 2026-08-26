ffd4617f13d26f006771399eac814c28e44fceaf
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-08-26 22:37:27 +0800

    Event: Remove getPeriod() helper method

--- src/main/java/Tasks/Event.java
+++ src/main/java/Tasks/Event.java
@@ -16,9 +16,10 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return String.format("[E]%s %s",
+        return String.format("[E]%s (from: %s to: %s)",
                 super.toString(),
-                this.getPeriod());
+                this.from,
+                this.to);
     }
 
     @Override
