4f983449f4a3923ef15d67dfba42053f829038da
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-08-27 00:08:38 +0800

    ToDo.java: Fix invalid return in getStorageFormat()

--- src/main/java/Tasks/ToDo.java
+++ src/main/java/Tasks/ToDo.java
@@ -12,6 +12,6 @@ public class ToDo extends Task {
 
     @Override
     public String getStorageFormat() {
-        return String.format("%s | T", super.getStorageFormat());
+        return String.format("%s | T |", super.getStorageFormat());
     }
 }
