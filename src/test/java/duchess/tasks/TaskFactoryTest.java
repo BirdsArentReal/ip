bf241545c6b4cd01d337d4ed1f8c6f1d8c596b57
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-08-29 15:03:11 +0800

    TaskFactoryTest.java: Add tests for creating tasks from commands

---
+++ src/test/java/duchess/tasks/TaskFactoryTest.java
@@ -0,0 +1,46 @@
+package duchess.tasks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import duchess.tasks.exceptions.TaskException;
+import org.junit.jupiter.api.Test;
+
+/** Tests command parsing and the persisted representations produced by {@link TaskFactory}. */
+class TaskFactoryTest {
+
+    @Test
+    void createFromCommand_todoCommand_returnsExpectedStorageFormat() throws TaskException {
+        Task task = TaskFactory.createFromCommand("todo read book");
+
+        assertEquals("0 | read book | T |", task.getStorageFormat());
+    }
+
+    @Test
+    void createFromCommand_deadlineCommand_returnsExpectedStorageFormat() throws TaskException {
+        Task task = TaskFactory.createFromCommand("deadline submit assignment /by 2024-12-30");
+
+        assertEquals("0 | submit assignment | D | /by 2024-12-30", task.getStorageFormat());
+    }
+
+    @Test
+    void createFromCommand_eventCommand_returnsExpectedStorageFormat() throws TaskException {
+        Task task = TaskFactory.createFromCommand("event team meeting /from 2024-10-15 /to 2024-10-16");
+
+        assertEquals("0 | team meeting | E | /from 2024-10-15 /to 2024-10-16",
+                task.getStorageFormat());
+    }
+
+    @Test
+    void createFromCommand_eventFromDateAfterToDate_throwsTaskException() {
+        String eventCreator = "event test /from 2027-12-12 /to 2020-12-12";
+        assertThrows(TaskException.class, () -> TaskFactory.createFromCommand(eventCreator));
+
+    }
+
+    @Test
+    void createDeadlineFromCommand_deadlineInvalidDateFormat_throwsTaskException() {
+        String deadlineCreator = "deadline a /by wrong date";
+        assertThrows(TaskException.class, () -> TaskFactory.createFromCommand(deadlineCreator));
+    }
+}
