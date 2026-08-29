121df94716f188d21de52e5646297f383ba255ce
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-08-29 23:27:42 +0800

    DuchessException.java: Add JavaDoc

--- src/main/java/duchess/ui/exceptions/DuchessException.java
+++ src/main/java/duchess/ui/exceptions/DuchessException.java
@@ -1,5 +1,8 @@
 package duchess.ui.exceptions;
 
+/**
+ * Represents an Exception due to the user command.
+ */
 public class DuchessException extends Exception {
     public DuchessException(String message) {
         super("Ohoho! " + message);
