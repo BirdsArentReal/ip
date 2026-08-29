20f554742644049dcee7b8fb0c4467dfad4bf74c
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-08-29 23:35:03 +0800

    CommandType.java: Add Egyptian-style braces

--- src/main/java/duchess/parse/CommandType.java
+++ src/main/java/duchess/parse/CommandType.java
@@ -4,19 +4,43 @@ public enum CommandType {
     LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT, BYE, UNKNOWN;
 
     public static CommandType parse(String input) {
-        if (input == null) return UNKNOWN;
+        if (input == null) {
+            return UNKNOWN;
+        }
 
         String s = input.stripLeading().toLowerCase();
-        if (s.startsWith("mark ")) return MARK;
-        if (s.startsWith("unmark ")) return UNMARK;
-        if (s.startsWith("delete ")) return DELETE;
-        if (s.startsWith("todo ")) return TODO;
-        if (s.startsWith("deadline ")) return DEADLINE;
-        if (s.startsWith("event ")) return EVENT;
+
+        // Commands with additional fields
+        if (s.startsWith("mark ")) {
+            return MARK;
+        }
+        if (s.startsWith("unmark ")) {
+            return UNMARK;
+        }
+        if (s.startsWith("delete ")) {
+            return DELETE;
+        }
+        if (s.startsWith("todo ")) {
+            return TODO;
+        }
+        if (s.startsWith("deadline ")) {
+            return DEADLINE;
+        }
+        if (s.startsWith("event ")) {
+            return EVENT;
+        }
 
         s = s.trim();
-        if (s.equals("list")) return LIST;
-        if (s.equals("bye")) return BYE;
+
+        // Commands without additional fields
+        if (s.equals("list")) {
+            return LIST;
+        }
+        if (s.equals("bye")) {
+            return BYE;
+        }
+
+        // Unrecognized commands.
         return UNKNOWN;
     }
 
