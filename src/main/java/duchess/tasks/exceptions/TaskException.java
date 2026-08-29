3a29438c51ee4c7d210927c643b5931ce58d469b
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-08-29 23:26:38 +0800

    TaskException.java: Add JavaDoc

--- src/main/java/duchess/tasks/exceptions/TaskException.java
+++ src/main/java/duchess/tasks/exceptions/TaskException.java
@@ -1,27 +1,56 @@
 package duchess.tasks.exceptions;
 
+/**
+ * Represents all exceptions related to creating a task.
+ */
 public class TaskException extends Exception {
     private TaskException(String message) {
         super("OOPS!!! " + message);
     }
 
-    public static TaskException declareEmptyDescription(String name) {
+    /**
+     * Creates a TaskException representing a failed creation operation due to
+     * an empty description.
+     *
+     * @param type The type of Task being created.
+     * @return The TaskException instance representing the reason for
+     *          the failed creation.
+     */
+    public static TaskException declareEmptyDescription(String type) {
         return new TaskException(String.format(
                 "The description of the %s cannot be empty.",
-                name
+                type
         ));
     }
 
-    public static TaskException declareInvalidCharacters(String cmd, String invalidChars) {
+    /**
+     * Creates a TaskException representing a failed creation operation due to
+     * invalid characters in the input.
+     *
+     * @param command The input string containing an invalid character.
+     * @param invalidChars The list of invalid characters.
+     * @return The TaskException instance representing the reason for
+     *          the failed creation.
+     */
+    public static TaskException declareInvalidCharacters(String command, String invalidChars) {
         return new TaskException(String.format(
                 "Your command contains invalid characters! \n"
                 + "Invalid Characters: %s\n"
                 + "Your command: %s",
                 invalidChars,
-                cmd
+                command
         ));
     }
 
+    /**
+     * Creates a TaskException representing a failed creation operation due to
+     * missing fields in the input command.
+     *
+     * @param command The input string with a missing field.
+     * @param fieldName The missing field.
+     * @return The TaskException instance representing the reason for
+     *          the failed creation.
+     */
     public static TaskException declareMissingField(String command, String fieldName) {
         return new TaskException(String.format(
                 "The %s command requires a %s field.\n"
@@ -32,14 +61,30 @@ public class TaskException extends Exception {
         ));
     }
 
-    public static TaskException declareUnrecognisedCommand(String cmd) {
+    /**
+     * Creates a TaskException representing a failed creation operation due to
+     * an unrecognized command.
+     *
+     * @param command The input string containing an unrecognized command.
+     * @return The TaskException instance representing the reason for
+     *          the failed creation.
+     */
+    public static TaskException declareUnrecognisedCommand(String command) {
         return new TaskException(String.format(
                 "The duchess does not recognise the task: \n"
                 + "  %s",
-                cmd
+                command
         ));
     }
 
+    /**
+     * Creates a TaskException representing a failed creation operation due to
+     * an invalid date format.
+     *
+     * @param dateStr The date string that is in the invalid format.
+     * @return The TaskException instance representing the reason for
+     *          the failed creation, and the correct date format.
+     */
     public static TaskException declareInvalidDateFormat(String dateStr) {
         return new TaskException(String.format(
                 "The date \"%s\" is not in a valid format. \n"
@@ -48,6 +93,15 @@ public class TaskException extends Exception {
         ));
     }
 
+    /**
+     * Creates a TaskException representing a failed creation operation due to
+     * an invalid date range.
+     *
+     * @param from The string representing the starting date.
+     * @param to The string representing the ending date.
+     * @return The TaskException instance representing the reason for
+     *          the failed creation.
+     */
     public static TaskException declareInvalidDateRange(String from, String to) {
         return new TaskException(String.format(
                 "Your event starts from %s, \n"
