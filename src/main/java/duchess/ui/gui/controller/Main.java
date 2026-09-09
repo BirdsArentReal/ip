366a4cd67a11149d6dd4d9d3ad220f331e8c2076
BirdsArentReal <51477059+BirdsArentReal@users.noreply.github.com>
2026-09-09 17:00:14 +0800

    Main.java: Add assertions

--- src/main/java/duchess/ui/gui/controller/Main.java
+++ src/main/java/duchess/ui/gui/controller/Main.java
@@ -1,6 +1,7 @@
 package duchess.ui.gui.controller;
 
 import java.io.IOException;
+import java.net.URL;
 
 import duchess.ui.Duchess;
 import duchess.ui.gui.components.MainWindow;
@@ -16,24 +17,33 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
+        Duchess duchess;
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-
-            AnchorPane ap = fxmlLoader.load();
-            Scene scene = new Scene(ap);
-            stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setBot(
-                    new Duchess("data", "duchess.txt")
-            );
-
-            stage.setTitle("Duke");
-            stage.setMinWidth(400.0);
-            stage.setMinHeight(600.0);
-
-            stage.show();
+            duchess = new Duchess("data", "duchess.txt");
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new IllegalStateException("Could not initialise task storage", e);
         }
+
+        URL fxmlResource = Main.class.getResource("/view/MainWindow.fxml");
+        assert (fxmlResource != null) : "MainWindow.fxml must be present in /resources/view/";
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlResource);
+
+        AnchorPane ap;
+        try {
+            ap = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not load MainWindow.fxml", e);
+        }
+
+        MainWindow mainWindow = fxmlLoader.getController();
+        assert (mainWindow != null) : "MainWindow.fxml must provide a MainWindow controller";
+        mainWindow.setBot(duchess);
+
+        stage.setScene(new Scene(ap));
+        stage.setTitle("Duke");
+        stage.setMinWidth(400.0);
+        stage.setMinHeight(600.0);
+        stage.show();
     }
 
 
